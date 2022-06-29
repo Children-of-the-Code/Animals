7ba46e8553a763662ac186ab56480f9006b0f83c
Lysette <lysette409@revature.net>
2022-06-29 11:00:44 -0500

    improved view inquiries and transactions

--- src/pages/InquiriesAdmin.js
+++ src/pages/InquiriesAdmin.js
@@ -6,6 +6,9 @@ export class InquiriesAdmin extends React.Component{
       this.state={
         allInquiries:[]
       }
+
+      this.setStatus = this.setStatus.bind(this);
+      this.refreshPage = this.refreshPage.bind(this);
     }
 
     componentDidMount(){
@@ -22,6 +25,39 @@ export class InquiriesAdmin extends React.Component{
       })
     }
 
+    refreshPage() {
+      window.location.reload(false);
+    }
+
+    setStatus(id, status){
+
+      let data = {
+        "inquiry_id": id,
+        "status": status
+      }
+
+      console.log(data);
+  
+      fetch("https://animalrescueproject.azurewebsites.net/inquiries/status", {
+              method: 'PUT',
+              mode: "cors",
+              headers:{
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(data)
+          })
+          .then(response=>response.json())
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+
+          this.refreshPage();
+
+    }
+
     render(){
       return(
         <div className="container">
@@ -34,14 +70,19 @@ export class InquiriesAdmin extends React.Component{
                     <p>ID: {inquiry.inquiry_id} | Username: {inquiry.user.username} | Animal Name: {inquiry.animal.name} | Status: {inquiry.status}</p>
                     { inquiry.status == "Pending"&&
                       <div className = "result-buttons">
-                        <button>Approve</button>
-                        <button>Deny</button>
-                        <button>Cancel</button>
+                        <button onClick={() => {this.setStatus(inquiry.inquiry_id,"Approved")}}>Approve</button>
+                        <button onClick={() => {this.setStatus(inquiry.inquiry_id,"Denied")}}>Deny</button>
+                        <button onClick={() => {this.setStatus(inquiry.inquiry_id,"Cancelled")}}>Cancel</button>
                       </div>
                     }
-                    { !inquiry.status == "Pending"&&
+                    { inquiry.status == "Approved"&&
                       <div className = "result-buttons">
-                        <button>Cancel</button>
+                        <button onClick={() => {this.setStatus(inquiry.inquiry_id,"Cancelled")}}>Cancel</button>
+                      </div>
+                    }
+                    { inquiry.status == "Denied"&&
+                      <div className = "result-buttons">
+                        <button onClick={() => {this.setStatus(inquiry.inquiry_id,"Cancelled")}}>Cancel</button>
                       </div>
                     }
                   </div>
